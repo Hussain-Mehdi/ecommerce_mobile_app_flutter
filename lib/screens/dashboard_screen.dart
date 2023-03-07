b7e27cbf67b559861195083e54b2e1fd7af30a9c
Hussain-Mehdi <hussainmehdibirmani47@gmail.com>
2023-03-07 23:19:37 +0500

    created dashboard screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -5,14 +5,88 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double sizeX = MediaQuery.of(context).size.height;
+    double sizeY = MediaQuery.of(context).size.width;
     return Material(
-      child: Stack(
-        children: [
-          Container(
-            decoration:
-                BoxDecoration(image: DecorationImage(image: AssetImage(""))),
-          )
-        ],
+      child: Container(
+        height: sizeX,
+        width: sizeY,
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("./images/men.jpg"))),
+            ),
+            Positioned(
+              bottom: 50,
+              right: 0,
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(50))),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: 200,
+                width: sizeY,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      colors: [
+                        Color(0xff7d5a98),
+                        Color(0xff3c2e48),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30))),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: SizedBox(
+                        width: 240,
+                        child: Text(
+                          "The Touch of Modern app can help. The tool offers everything from clothing, watches and barware to beef jerky and artwork. It’s virtually a one-stop shop for modern man gifting.",
+                          style: TextStyle(
+                            textBaseline: TextBaseline.alphabetic,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      height: 55,
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Center(
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
