05bec4ef07b1a3f559a8e4e78de2b6e5fcebe829
Hussain-Mehdi <hussainmehdibirmani47@gmail.com>
2023-03-09 23:46:45 +0500

    created filter screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/catalogue_screen.dart';
 import 'screens/clothing_screen.dart';
 import 'screens/dashboard_screen.dart';
+import 'screens/filter_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/signin_screen.dart';
 import 'screens/verification_screen.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ecommerce App',
-      home: ClothingScreen(),
+      home: FilterScreen(),
     );
   }
 }
---
+++ lib/screens/filter_screen.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key});
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  @override
+  double start = 30;
+  double end = 50;
+  List<DropdownMenuItem> dropDownItems = [
+    DropdownMenuItem(child: Text("Jeans")),
+    DropdownMenuItem(child: Text("Shirts")),
+    DropdownMenuItem(child: Text("T-Shirts")),
+    DropdownMenuItem(child: Text("Tops")),
+    DropdownMenuItem(child: Text("Caps")),
+    DropdownMenuItem(child: Text("Jackets")),
+  ];
+
+  int selectedValue = 0;
+  Widget build(BuildContext context) {
+    return Material(
+        child: Column(
+      children: [
+        Container(
+            height: 120,
+            child: Stack(children: [
+              Container(
+                height: 120,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Color(0xff3c2e48),
+                    Color.fromARGB(255, 107, 71, 138)
+                  ]),
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 8.0, right: 8.0, top: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                            size: 20,
+                          )),
+                      Text("Clothings",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700)),
+                      IconButton(
+                        onPressed: () {},
+                        icon: ImageIcon(AssetImage("./images/filter.png")),
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ])),
+        RangeSlider(
+            values: RangeValues(start, end),
+            labels: RangeLabels('y', 'e'),
+            min: 10,
+            max: 80,
+            onChanged: (value) {
+              setState(() {
+                start = value.start;
+                end = value.end;
+              });
+            }),
+        Padding(
+          padding: const EdgeInsets.only(left: 50.0),
+          child: Row(
+            children: [
+              Container(
+                height: 60,
+                width: 120,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10)),
+                    color: Color.fromARGB(255, 247, 247, 247)),
+                child: Center(
+                  child: Text(
+                    "\$${start.toStringAsFixed(0)}",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                height: 60,
+                width: 120,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10)),
+                    color: Color.fromARGB(255, 247, 247, 247)),
+                child: Center(
+                  child: Text(
+                    "\$${end.toStringAsFixed(0)}",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20, top: 10),
+          child: DropdownButtonFormField(
+            itemHeight: 60,
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                enabledBorder:
+                    UnderlineInputBorder(borderSide: BorderSide.none),
+                focusedBorder:
+                    UnderlineInputBorder(borderSide: BorderSide.none)),
+            items: <String>[
+              "Jeans",
+              "Shirts",
+              "T-Shirts",
+              "Tops",
+              "Caps",
+              "Jackets"
+            ].map((String value) {
+              return DropdownMenuItem(
+                child: Text(value),
+                value: value,
+              );
+            }).toList(),
+            onChanged: (value) {},
+            hint: Text(selectedValue.toString()),
+          ),
+        ),
+      ],
+    ));
+  }
+}
