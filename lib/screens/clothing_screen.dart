bd635a2ceff35907f45ce946f76df851ba010020
Hussain-Mehdi <hussainmehdibirmani47@gmail.com>
2023-03-09 12:48:46 +0500

    clothing in progress

--- lib/screens/clothing_screen.dart
+++ lib/screens/clothing_screen.dart
@@ -50,12 +50,10 @@ class _ClothingScreenState extends State<ClothingScreen> {
                                 fontSize: 18,
                                 fontWeight: FontWeight.w700)),
                         IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.filter,
-                              color: Colors.white,
-                              size: 20,
-                            )),
+                          onPressed: () {},
+                          icon: ImageIcon(AssetImage("./images/filter.png")),
+                          color: Colors.white,
+                        ),
                       ],
                     ),
                   ),
@@ -168,8 +166,7 @@ class _ClothingScreenState extends State<ClothingScreen> {
   }
 
   Widget createFeaturedItems() {
-    List<Widget> catalogues = [];
-    List<String> featuredData = [
+    List<String> clothingData = [
       './images/1.png',
       './images/2.png',
       './images/3.png',
@@ -187,101 +184,65 @@ class _ClothingScreenState extends State<ClothingScreen> {
     ];
 
     return GridView.builder(
-      itemCount: featuredData.length,
+      itemCount: clothingData.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          childAspectRatio: MediaQuery.of(context).size.width /
-              (MediaQuery.of(context).size.height / 1),
-          mainAxisSpacing: 60,
-          crossAxisCount: 2),
+          mainAxisSpacing: 30, crossAxisCount: 2),
       itemBuilder: (context, index) {
         return Padding(
-          padding: const EdgeInsets.only(left: 20.0),
-          child: Stack(
+          padding: const EdgeInsets.only(left: 20.0, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                width: 140,
-                height: 230,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(15)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Stack(children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 150,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                                image: AssetImage(featuredData[index]))),
-                      ),
-                      Positioned(
-                        top: 15,
-                        child: Container(
-                          width: 50,
-                          height: 25,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(20),
-                                  bottomRight: Radius.circular(20)),
-                              color: Color.fromARGB(195, 244, 67, 54)),
-                          child: Center(
-                            child: Text(
-                              "50% off",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 10,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ]),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        IconButton(
-                            padding: EdgeInsets.all(0),
-                            constraints: BoxConstraints(),
-                            onPressed: () {},
-                            icon: Icon(Icons.star_outline, size: 15)),
-                        IconButton(
-                            padding: EdgeInsets.all(0),
-                            constraints: BoxConstraints(),
-                            onPressed: () {},
-                            icon: Icon(Icons.star_outline, size: 15)),
-                        IconButton(
-                            padding: EdgeInsets.all(0),
-                            constraints: BoxConstraints(),
-                            onPressed: () {},
-                            icon: Icon(Icons.star_outline, size: 15)),
-                        IconButton(
-                            padding: EdgeInsets.all(0),
-                            constraints: BoxConstraints(),
-                            onPressed: () {},
-                            icon: Icon(Icons.star_outline, size: 15)),
-                      ],
-                    ),
-                    Container(
-                        margin: EdgeInsets.only(left: 10, right: 10),
-                        width: 130,
-                        height: 30,
-                        child: Text(
-                            "A quick brown clever fox jumps over the lazy dog",
-                            style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500,
-                                overflow: TextOverflow.fade))),
-                    Container(
-                        margin: EdgeInsets.only(left: 10, right: 10),
-                        width: 130,
-                        child: Text("\$120.00",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w800))),
-                  ],
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      image: DecorationImage(
+                          image: AssetImage(clothingData[index]))),
                 ),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      padding: EdgeInsets.all(0),
+                      constraints: BoxConstraints(),
+                      onPressed: () {},
+                      icon: Icon(Icons.star_outline, size: 15)),
+                  IconButton(
+                      padding: EdgeInsets.all(0),
+                      constraints: BoxConstraints(),
+                      onPressed: () {},
+                      icon: Icon(Icons.star_outline, size: 15)),
+                  IconButton(
+                      padding: EdgeInsets.all(0),
+                      constraints: BoxConstraints(),
+                      onPressed: () {},
+                      icon: Icon(Icons.star_outline, size: 15)),
+                  IconButton(
+                      padding: EdgeInsets.all(0),
+                      constraints: BoxConstraints(),
+                      onPressed: () {},
+                      icon: Icon(Icons.star_outline, size: 15)),
+                ],
+              ),
+              Container(
+                  margin: EdgeInsets.only(left: 10, right: 10),
+                  width: 130,
+                  height: 30,
+                  child: Text(
+                      "A quick brown clever fox jumps over the lazy dog",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                          overflow: TextOverflow.fade))),
+              Container(
+                  margin: EdgeInsets.only(left: 10, right: 10),
+                  width: 130,
+                  child: Text("\$120.00",
+                      style: TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w800))),
               Positioned(
                 right: 30,
                 top: 130,
