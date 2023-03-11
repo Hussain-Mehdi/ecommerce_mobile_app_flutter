f3f20c062002bc55bb1048f91c1b0bebe864e7b2
Hussain-Mehdi <hussainmehdibirmani47@gmail.com>
2023-03-11 22:02:32 +0500

    checkout screen completed

--- lib/screens/checkout_screen.dart
+++ lib/screens/checkout_screen.dart
@@ -49,13 +49,21 @@ class CheckoutScreenState extends State<CheckoutScreen> {
               ),
             ])),
         Padding(
-          padding: const EdgeInsets.only(left: 20.0, top: 20),
-          child: Text(
-            "Catagory",
-            style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87),
+          padding: const EdgeInsets.only(left: 20.0, top: 20, bottom: 10),
+          child: Row(
+            children: [
+              Icon(Icons.location_on_outlined),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  "Payment Method",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+              ),
+            ],
           ),
         ),
         Padding(
@@ -112,17 +120,26 @@ class CheckoutScreenState extends State<CheckoutScreen> {
         ),
         Padding(
           padding: const EdgeInsets.only(left: 20.0, top: 20),
-          child: Text(
-            "Catagory",
-            style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87),
+          child: Row(
+            children: [
+              Icon(Icons.delivery_dining_outlined),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  "Delivery Method",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+              ),
+            ],
           ),
         ),
         Padding(
-          padding: EdgeInsets.only(left: 20, right: 20),
+          padding: EdgeInsets.only(left: 20, right: 20, top: 10),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
                 height: 100,
@@ -139,24 +156,26 @@ class CheckoutScreenState extends State<CheckoutScreen> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 18.0, right: 18),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        "B-17 Islamabad,",
-                        style: TextStyle(
-                            fontSize: 13, fontWeight: FontWeight.w800),
+                      Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('./images/easypaisa.png'))),
                       ),
                       Text(
-                        "Block B Main Markaz",
+                        "\$16.00",
                         style: TextStyle(
-                          fontSize: 12,
-                        ),
+                            color: Color.fromARGB(255, 93, 57, 121),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800),
                       ),
                       Text(
-                        "Margalla Heights Palaza Second floor",
+                        "1-2 days",
                         style: TextStyle(
-                          fontSize: 12,
-                        ),
+                            fontSize: 14,
+                            color: Color.fromARGB(255, 136, 136, 136)),
                       ),
                     ],
                   ),
@@ -177,24 +196,26 @@ class CheckoutScreenState extends State<CheckoutScreen> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 18.0, right: 18),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        "B-17 Islamabad,",
-                        style: TextStyle(
-                            fontSize: 13, fontWeight: FontWeight.w800),
+                      Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('./images/sadapay.png'))),
                       ),
                       Text(
-                        "Block B Main Markaz",
+                        "\$18.00",
                         style: TextStyle(
-                          fontSize: 12,
-                        ),
+                            color: Color.fromARGB(255, 93, 57, 121),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800),
                       ),
                       Text(
-                        "Margalla Heights Palaza Second floor",
+                        "1-2 days",
                         style: TextStyle(
-                          fontSize: 12,
-                        ),
+                            fontSize: 14,
+                            color: Color.fromARGB(255, 136, 136, 136)),
                       ),
                     ],
                   ),
@@ -215,30 +236,187 @@ class CheckoutScreenState extends State<CheckoutScreen> {
                 child: Padding(
                   padding: const EdgeInsets.only(left: 18.0, right: 18),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        "B-17 Islamabad,",
-                        style: TextStyle(
-                            fontSize: 13, fontWeight: FontWeight.w800),
+                      Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('./images/master.png'))),
                       ),
                       Text(
-                        "Block B Main Markaz",
+                        "\$20.00",
                         style: TextStyle(
-                          fontSize: 12,
-                        ),
+                            color: Color.fromARGB(255, 93, 57, 121),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800),
                       ),
                       Text(
-                        "Margalla Heights Palaza Second floor",
+                        "1-2 days",
                         style: TextStyle(
-                          fontSize: 12,
-                        ),
+                            fontSize: 14,
+                            color: Color.fromARGB(255, 136, 136, 136)),
                       ),
                     ],
                   ),
                 ),
               ),
             ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, top: 20, bottom: 10),
+          child: Row(
+            children: [
+              Icon(Icons.payment_outlined),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  "Payment Method",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Container(
+            height: 100,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+                boxShadow: [
+                  BoxShadow(
+                      color: Color.fromARGB(41, 194, 194, 194),
+                      blurRadius: 0.5,
+                      spreadRadius: 1),
+                ]),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 18.0, right: 18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "B-17 Islamabad,",
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w800),
+                      ),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Change >",
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.w800),
+                          ))
+                    ],
+                  ),
+                  Text(
+                    "Block B Main Markaz",
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    "Margalla Heights Palaza Second floor",
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(top: 60),
+            decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Color.fromARGB(38, 190, 190, 190),
+                      spreadRadius: 2,
+                      blurRadius: 1)
+                ],
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.elliptical(20, 20),
+                    topRight: Radius.elliptical(20, 20)),
+                color: Colors.white),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Items",
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "\$290.00",
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Delivery",
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "\$10.00",
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Total Price",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        "\$300.00",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 20, top: 10, right: 20),
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Center(
+                      child: Text(
+                        "Pay",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         )
       ],
